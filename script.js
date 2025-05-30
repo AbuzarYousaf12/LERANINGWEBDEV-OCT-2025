32465117a20d85f8bb0ba9580f39c95f3bea890b
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-05-30 17:13:45 +0500

    CODE RUNNER EXTENSION WITH RUN TERMINAL ON

--- script.js
+++ script.js
@@ -1,11 +1,76 @@
-console.log("Hey this is tutorial 2 of sigma web dev course")
+        console.log("Hey this is tutorial 2 of sigma web dev course")
 
-        var a = 5;
-        var b = 6;
+
+        let i = 5; // GLOBAL VARIABLE LET BLOCCK SCOPE HOTA HE 
+        var a = 51; // GLOBAL SCOPE
+        var b = 6; // VAR JO HOTA HE WO GLOBALLY SCOPE HOTA HE BLOCK SCOPE NAHE HOTA HE
+
         var c = "Harry"
         var _c = "Shubam"
         // var 55a = "Rohan"; // INVALID
 
-        console.log(a + b + 8);
+        // JAVASCRIPT ME VAR ZIADA USE NAE HOTA
+
+        // console.log(a + b + 8);
 
         console.log(typeof a, typeof b, typeof c)  // CODE TYPE JAN NE KELEYE
+
+
+        // PRIMITVE DATA TYPES JOKE BAKI JAVASCIPRT KE BUILDING BLOCKS HOTE HE
+        // Null // Number // String // Symbol // Undefined // Boolean // BigInt
+
+        
+{
+    let i = 66;
+    console.log (i)
+}
+console.log (i)
+
+{
+    var a = 55;
+    console.log (a)
+}
+
+console.log (a) // VAR USE KIA ISLIE JB a = 55 KIA TB BAHR BHI 55 HI ARHA HE KIO KE GLOBAL SCOPE HE VAR ISLIE LET USE KERTE H
+        const a1 = 6;
+
+        // const a1 = 9; // NOT ALLOWED 
+
+        // a1 = 19; //NOT ALLOWED 
+
+        a2 = a1 + 9;
+
+        // console.log(a2) 
+
+
+
+
+
+        let x = "Harry Bro";
+        let y = 22;
+        let z = 3.55;
+        const p = true;
+        let q = undefined;
+        let r = null;
+
+        console.log (x,y,z,p,q,r)
+        console.log (typeof x,typeof y,typeof z,typeof p,typeof q,typeof r)
+
+
+        // OBJECT OBJECT JAVASCRIPT ME KEYVALUE PAIRS KA COMBINATION
+
+        let o = {
+            name: "Harry",
+            "Job role": "Web Developer", // KEY VALUE PAIR HE YE
+            "Is Handsome": "true" // KEY VALUE PAIRS HEN YE JO KE COMMA SE DAL SKTE HN
+        }
+
+        console.log (o)
+
+        o.salary = "100 CRORES"
+
+        console.log(o)
+
+        o.salary = "500 CRORES"
+
+        console.log(o)
