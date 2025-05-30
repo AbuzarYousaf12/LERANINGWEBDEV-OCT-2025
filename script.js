72208d0c66a41a47f9fcd6ef243813ecb8f6eb4b
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-05-30 15:43:36 +0500

    LEARNING JAVASCRIPT

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hey this is tutorial 2 of sigma web dev course")
+
+        var a = 5;
+        var b = 6;
+        var c = "Harry"
+        var _c = "Shubam"
+        // var 55a = "Rohan"; // INVALID
+
+        console.log(a + b + 8);
+
+        console.log(typeof a, typeof b, typeof c)  // CODE TYPE JAN NE KELEYE
