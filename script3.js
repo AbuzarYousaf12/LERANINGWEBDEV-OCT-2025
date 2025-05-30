d0955de5ed0e0cd6beb04692638305aa9495d45f
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-05-30 18:37:26 +0500

    LOOPS

--- script3.js
+++ script3.js
@@ -8,6 +8,42 @@ let a = 1;
     
 // }
 
-for (let i = 0; i < 100; i++) {
-    console.log (a+i);
+
+
+//FOR LOOP
+    //INITIALIZATION     //CONDITION       //
+// for (let i = 0;          i < 100;          i++) {
+//     console.log (a+i);
+// }
+
+
+// FOR IN LOOP
+
+// let obj = {
+//     name: "Harry",
+//     role: "Programmer",
+//     company: "CodeWithHaryy AI"
+//     //KEY OF OBJECT : // ELEMENT
+// }
+
+
+// // FOR IN LOOP
+// for (const key in obj) {
+//      const element = obj[key];
+//      console.log(key, element)
+// }
+
+
+// // FOR OF LOOP
+// for (const c of "Harry") {
+//     console.log(c)
+// }
+
+
+// WHILE LOOP
+let z = 5;
+while (z<6) {
+    console.log(z)
+    z++;
+    
 }
