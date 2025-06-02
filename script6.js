a7f0f4c8cc74e39db42192a8e5ce1f4dbfb53c0f
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-02 15:41:13 +0500

    Learning ARRAYS

---
+++ script6.js
@@ -0,0 +1,10 @@
+let arr = [1, 2, 4, 5, 7]
+// Index   0, 1, 2, 3, 4
+
+console.log(arr);
+console.log(arr.length);
+
+arr[0] = 5666;   // ARRAY CHANGE HOJAYEGA LEKIN YE STRING ME NAE HOTA ARRAY CHANGE HOJAYA HE STRING CHANGE NAE HOTA HE
+console.log(arr[0])
+console.log(arr[2])
+console.log(arr[4])
