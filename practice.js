f34ee1590c0f7df790ae6ab809eec928dea78e26
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-02 12:24:49 +0500

    Practicejs

---
+++ practice.js
@@ -0,0 +1,15 @@
+console.log("Hey I am Learning Javascript")
+
+let a = 35;
+
+if (a>15) {
+console.log("Congratulations You Are Pass")    
+}
+else if (age>30) {
+    console.log("Congratulations You Are A Topper")
+}
+else {
+    console.log("You are Fail")
+}
+
+var a = 
