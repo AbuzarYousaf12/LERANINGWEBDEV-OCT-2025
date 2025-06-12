e2eeaeb9f4f3d467ab2c9efc866c9aaac7ad78a6
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-12 19:08:40 +0500

    12 June

--- eventsbubblingsetintervalsettimeout.js
+++ eventsbubblingsetintervalsettimeout.js
@@ -1,1 +1,9 @@
 console.log("EVENTS EVENT BUBBLING SET INTERVAL & SET TIMEOUT IN JAVASCRIPT")
+
+let button = document.getElementById("btn")
+
+button.addEventListener("click", ()=>{
+    // alert('I was clicked') 
+    // document.querySelector(".box").innerHTML = "<b>Yayy you were clicked</b> Enjoy your click!"
+    
+})
