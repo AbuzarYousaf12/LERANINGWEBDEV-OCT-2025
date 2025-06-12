7330122fe6d150c83337ffcff8194802d6d671a1
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-12 17:08:51 +0500

    INSERTING & REMOVING IN JAVASCRIPT

--- javascriptdom2.js
+++ javascriptdom2.js
@@ -18,3 +18,15 @@ document.querySelectorAll(".box").forEach(e =>{
     e.style.backgroundColor = "green";
 })
 
+document.getElementsByTagName("div") // HTML COLLECTION RETURN KERTA HE YANI JITNE BHI DIV HONGE SB KE SB SELECT HONGE
+
+e = document.getElementsByTagName("div")
+
+e[4].matches('#redbox') // YANI E KA 4TH INDEX MATCH KERTA HE K NAE KERTA HE ISLIE TRUE AYEGA
+
+e[3].closest('#redbox') // YANI NEAREST ELEMENT KONSA HE JO IS CSS SELECTOR KO MATCH KERTA HE ?? PEHLE KHUD ELEMENT PR CHECK KEREGA PHIR PARENT PER JAYEGA PHRI PARENT PER JAYEGA JB TK KUCH MATCH NA HOJAY PARENT TO PARENT JATA RHEGA..
+
+document.querySelector(".container").contains(e[2]) // HA CONTAIN KERTA HE ISLIE TRUE AJAYEGA 
+document.querySelector(".container").contains(e[0]) // HA CONTAIN KERTA HE ISLIE TRUE AJAYEGA 
+document.querySelector(".container").contains(document.querySelector("body")) // NAE CONTAIN KERTA HE ISLIE FALSE AYEGA
+document.querySelector("body").contains(document.querySelector(".container")) // HA TRUE BATAYEGA KIOKE BODY KE ANDAR CONTAINER WALA TAG HE ISLIE
