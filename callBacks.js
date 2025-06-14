02d4301d288c7935c62141048f333b96f71478e7
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-14 16:02:01 +0500

    CALL BACKS & PROMISES IN JAVASCRIPT

---
+++ callBacks.js
@@ -0,0 +1,11 @@
+// CALLBACK FUNTION IS LIE ISTEMAL HOTA HE JB AP EIK FUNCTION KO CALL KERNA CHAHTE HO JB WO EXECUTE HO
+// PROMISE BHT IMPORTANT CONCEPT HE ASYNC ME
+
+console.log("Harry is a hacker")
+console.log("Rohan is a hecker")
+
+setTimeout(() => {
+    console.log("I am inside settimeout")
+}, 2000);
+
+console.log("The End")
