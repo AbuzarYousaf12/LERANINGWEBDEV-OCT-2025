8b3b2fc83770e9f1dac6469c5013f6b68c459cae
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-14 17:06:30 +0500

    async&await in js

---
+++ async&await.js
@@ -0,0 +1,11 @@
+function getData (){
+
+}
+
+console.log("Loading Modules")
+
+console.log("Do Something Else")
+
+console.log("Load Data")
+
+console.log("Process Data")
--- promises.js
+++ promises.js
@@ -13,9 +13,32 @@ let prom1 = new Promise((resolve, reject) => {
     }
 })
 
-prom1.then((a) => {
-    console.log(a)
-}).catch((err) => {         // ERROR CATCH KERNE SE CONSOLE.LOG ME ERROR NAE DIKHEGA.
-    console.log(err)
+// prom1.then((a) => {
+//     console.log(a)
+// }).catch((err) => {         // ERROR CATCH KERNE SE CONSOLE.LOG ME ERROR NAE DIKHEGA.
+//     console.log(err)
+// })
+
+
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a<0.1) {
+        reject("No random runmber was not supporting you 2")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes I am done 2")
+            resolve("Harry")
+        }, 3000);
+    }
 })
 
+// PROMISE KE SETTLE HONE KA MTLB HE AGR ALLSETTLED USE KERENGE TO CHAHYE RESOLVE HO YA REJECT PROMISES CHLJAYENGE PROMISE RACE YE HE KE JO BHI VALUE SETTLE HOGYI USKI VALUE MILEGI
+
+let p3 = Promise.all([prom1, prom2])
+p3.then((a) => {
+    console.log(a)
+}).catch((err)=> {
+    console.log(err)
+})
