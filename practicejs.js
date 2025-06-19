6c5cba1c532e08e621f20d86655adc28ed4dc288
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-19 14:24:56 +0500

    PRACTICE

--- practicejs.js
+++ practicejs.js
@@ -1,1 +1,257 @@
-console.log("PRACTICING JAVASCRIPT 18-JUNE-2025")
+////////// ***** VIDEO 1 JAVASCRIPT ***** //////////
+
+
+
+
+
+// console.log("VIDEO NUMBER 1 JAVASCRIPT")
+
+// console.log("PRACTICING JAVASCRIPT 19-JUNE-2025")
+
+// alert("This is alert")
+
+
+// var a = prompt("Enter Your First Number")
+
+// var b = prompt("Enter Your Second Number")
+
+// let isTrue = confirm("Are u sure you want to leave this page and burn your computer")
+
+// console.log(a+b)
+
+// if(isTrue){
+//     console.log("YOUR COMPUTER IS BLASTING")
+// }
+// else {
+//     console.log("YOUR COMPUTER IS NOT BLASTING")
+// }
+
+// let c = 59;
+
+// console.log("YOU NUMBER IS ", + a)
+
+
+
+
+////////// ***** VIDEO 2 JAVASCRIPT ***** //////////
+
+
+
+
+
+// console.log("VIDEO NUMBER 2 JAVASCRIPT")
+
+// let a = 5;
+
+// let b = 6;
+
+// let c = "Harry";
+
+// var d = 19;
+
+// // var 55a = "Rohan" // NOT ALLOWED
+
+// {
+//     let a = 16
+//     console.log(a)
+// }
+
+// console.log(a)
+
+// // * ~Block scope means that variables declared inside a block { ... } cannot be accessed from outside that block.~ * //
+
+
+// {
+//     var d = 15;
+//     console.log(d)
+// }
+
+// console.log(d
+
+// )
+
+// console.log(a + b + 8)
+// console.log(typeof a, typeof b, typeof c)
+
+// // const a1 = 16;
+// // a1 = a1 + 15; // NOT ALLOWED BECAUSE a1 IS CONSTANT
+
+// *~PRIMITIVE DATA TYPES~* //
+// *~LET KA SCOPE BLOCK HOTA HE AUR US SE JO BHI VARIABLES BNTE HEN WO BLOCK SCOPE KEHLATE HEN~* //
+
+// let e = "Harry";
+// let t = 95;
+// let r = 3.55;
+// let o = true;
+// let p = null;
+// let q = undefined;
+
+// console.log(q, e, t, r, o, p)
+// console.log(typeof q,typeof e,typeof t,typeof r,typeof o,typeof p)
+
+// // *~OBJECTS IN JAVASCRIPT~* //
+
+// let obj = {
+//     "Job role": "Programmer",
+//     "Job code": 54600
+// }
+
+// console.log(obj)
+
+// // AGR KUCH CHANGE KERNA CHAHYTE THEN
+
+// obj.salary = "500CRORES";
+
+// console.log(obj)
+
+// // *~BOOLEAN VALUES~* //
+
+// // true or false are boolean values
+
+
+
+
+////////// ***** VIDEO 3 JAVASCRIPT ***** //////////
+
+
+
+
+
+// console.log("VIDEO NUMBER 3 JAVASCRIPT")
+
+// *~IF-ELSE CONDITION IN JAVASCRIPT~* //
+
+// let age = 18;
+// let grace = 2;
+
+// if (age != 0) {
+//     console.log("You cannot Drive")
+// }
+// else if (age > 18) {
+//     console.log("You can Drive")
+// }
+// else {
+//     console.log("You are eligible")
+// }
+
+// *~OPERATORS IN JAVASCRIPT~* //
+
+
+// *~ARITHMETIC OPERATORS IN JAVASCRIPT~* //
+
+
+// console.log(age + grace)              // ADDITION
+// console.log(age - grace)              // SUBTRACTION
+// console.log(age * grace)              // MULTIPLICAITON
+// console.log(age / grace)              // DIVISION
+// console.log(age % grace)              // MODULUS
+// console.log(age ** grace)             // EXPONENTIATION
+// console.log(age ++ grace)             // INCREMENT
+// console.log(age -- grace)             // DECREMENT
+
+
+// *~ASSIGNMENT OPERATORS IN JAVASCRIPT~* //
+
+
+// =	x = y	x = y
+// +=	x += y	x = x + y
+// -=	x -= y	x = x - y
+// *=	x *= y	x = x * y
+// /=	x /= y	x = x / y
+// %=	x %= y	x = x % y
+// **=	x **= y	x = x ** y
+
+// let realAge = 19;
+// age += realAge
+// console.log(age)
+// age -= realAge
+// console.log(age)
+// age *= realAge
+// console.log(age)
+// age /= realAge
+// console.log(age)
+// age %= realAge
+// console.log(age)
+// age **= realAge
+// console.log(2 ** 3);  // 8   → 2 * 2 * 2
+// console.log(age)
+
+
+// *~COMPARISON OPERATORS IN JAVASCRIPT~* //
+
+
+// ==	Equal to	5 == '5' ✅
+// ===	Strict equal (type + value)	5 === '5' ❌
+// !=	Not equal	5 != '5' ❌
+// !==	Strict not equal	5 !== '5' ✅
+// >	Greater than	5 > 3 ✅
+// <	Less than	5 < 3 ❌
+// >=	Greater than or equal to	5 >= 5 ✅
+// <=	Less than or equal to	5 <= 5 ✅
+
+
+
+// *~LOGICAL OPERATORS IN JAVASCRIPT~* //
+
+
+// &&	Logical AND	true && false ❌ 
+// ||  Logical OR false || false ❌
+// !	Logical NOT	!true ❌
+
+
+// *~BITWISE OPERATORS IN JAVASCRIPT~* //
+
+
+// .. ?? ADVANCED NOT LEARNED NOW ?? .. //
+
+
+// *~TERNARY OPERATORS IN JAVASCRIPT~* //
+
+
+// condition ? valueIfTrue : valueIfFalse;
+
+// let agee = 17;
+// let result = (agee >= 18) ? 'Adult' : 'Minor';
+// console.log(result)
+
+// let z = 6;
+// let l = 8;
+// let w = z > l ? (z - l) : (l - z)
+// console.log(w)
+
+//  translates to:
+// if (z > l)
+//     let w = z - l;
+// }
+// else {
+//     let w = l - z;
+// }
+
+
+// *~TYPE OPERATORS IN JAVASCRIPT~* //
+
+
+// typeof 'Hello';     // "string"
+// [1, 2, 3] instanceof Array; // true
+
+
+// *~SPREAD & REST OPERATORS IN JAVASCRIPT~* //
+
+
+// let arr = [1, 2, 3];
+// let newArr = [...arr, 4, 5];
+
+
+// function sum(...numbers) {
+//   return numbers.reduce((acc, num) => acc + num, 0);
+// }
+
+
+
+
+
+
+
+
+
+
