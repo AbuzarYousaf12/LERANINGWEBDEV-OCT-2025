739c024a23f661c9735f1a9330366bf948d38968
Abuzar Yousaf <abuzaryousaf.123456789@gmail.com>
2025-06-21 13:00:56 +0500

    BRO CODE JAVASCRIPT TUTORIALS

---
+++ brocode.js
@@ -0,0 +1,6 @@
+// console.log("This is Course of Javascript By Bro Code")
+
+// window.alert("This is course of javascript by bro code")
+
+document.getElementById("myH1").textContent = "Hello";
+document.getElementById("myP").textContent = `I am a Paragraph`
